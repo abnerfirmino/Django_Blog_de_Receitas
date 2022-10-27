5d2000b56514be395a2636dd382321c8780ae2a6
Abner Firmino <abnercasteliano@outlook.com>
2022-10-27 19:44:20 -0300

    template recipe-detail criado

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views
 
 # URLS do app recipes
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 ]
